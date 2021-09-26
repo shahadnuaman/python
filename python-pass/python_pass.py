c5b26fa8916dc5198d3031fe5c50256d14e57bb9
shahadnuaman <61418780+shahadnuaman@users.noreply.github.com>
2021-09-26 08:48:20 +0300

    Add files via upload

---
+++ python-pass/python_pass.py
@@ -0,0 +1,26 @@
+"""
+Instructions:
+
+1. Create a class named ReversedString that inherits from StringOperations class
+2. Implement the function reverse
+3. reverse function should be a one liner function that returns the reverse string to_be_reversed
+4. Instantiate the class ReversedString
+5. Print to show your function implementation result
+"""
+
+
+class StringOperations:
+    # def reverse(self, *, to_be_reversed: str = None):
+    #     RETURN NotImplemented('This method need to be implemented')
+
+
+def reverse(str):
+    print(str)
+        return str[::-1]
+        
+class ReversedString(StringOperations):
+
+     name  = 'tseT'
+    result = reverse(name)
+    print(result)
+     
